7d48d1a98a09b9b29fa288d0963550eab512547e
Benjamin <benjaminsara329@gmail.com>
2022-08-23 15:10:50 +0200

    Début du projet

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html mang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-Do List</title>
+</head>
+<body>
+    <div class="main-section">
+        <div class="add-section">
+            <form action="">
+                <input type="texte"
+                       name="titre"
+                       placeholder="Ce champs est obligatoire"/>
+                <button type="submit">Ajouter &nbsp;<span>&#43;</span></button>
+            </form>
+        </div>
+    </div>
+
+</body>
+</html>
